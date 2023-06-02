c58ad0586c25c280bffa3648da28ed40b8f4380a
Falk Hoppe <falk@hey.com>
2023-06-02 09:00:05 +0200

    initialize staggard component library

---
+++ deps.ts
@@ -0,0 +1,8 @@
+export { assertEquals } from "https://deno.land/std@0.190.0/testing/asserts.ts";
+
+export {
+  attr,
+  type AttributeValue,
+  html,
+  renderToString,
+} from "https://deno.land/x/staggard@0.0.29/main.ts";
