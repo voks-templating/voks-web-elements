028c6b8dacff6315227f6dcdb4fc74892cf1cb04
Falk Hoppe <falk@hey.com>
2023-06-05 16:21:57 +0200

    feat: add datalist element

---
+++ html/datalist.ts
@@ -0,0 +1,19 @@
+import { html, HTMLTemplate } from "../deps.ts";
+import { attributeList } from "./element_helper.ts";
+import { HTMLGlobalAttributes } from "./global_attributes.ts";
+
+export type DatalistAttributes = HTMLGlobalAttributes;
+
+export interface DatalistElementOptions {
+  classes?: string[];
+  attributes?: DatalistAttributes;
+}
+
+export const datalist = (
+  content: string | HTMLTemplate,
+  { attributes, classes }: DatalistElementOptions = {},
+) =>html`<datalist ${
+  attributeList<DatalistAttributes>(attributes, classes)
+}>${content}</datalist>`;
+
+  
---
+++ html/datalist_test.ts
@@ -0,0 +1,34 @@
+import { assertEquals } from "asserts";
+import { html, renderToString } from "../deps.ts";
+import { datalist } from "./datalist.ts";
+
+Deno.test("datalist tag element", async (t) => {
+  await t.step("datalist without attributes", async () => {
+    const actual = datalist("Content");
+
+    const expected = `<datalist>Content</datalist>`;
+
+    const rendered = await renderToString(actual, { minify: true });
+    assertEquals(rendered, expected);
+  });
+
+  await t.step("datalist with attributes", async () => {
+    const actual = datalist(
+      html`<option value="Chrome"></option>
+    <option value="Firefox"></option>
+    <option value="Opera"></option>
+    <option value="Safari"></option>
+    <option value="Microsoft Edge"></option>`,
+      {
+        attributes: {
+          id: "browsers", // id is a global attribute
+        },
+      },
+    );
+
+    const expected =
+      `<datalist id="browsers"><option value="Chrome"></option> <option value="Firefox"></option> <option value="Opera"></option> <option value="Safari"></option> <option value="Microsoft Edge"></option></datalist>`;
+    const rendered = await renderToString(actual, { minify: true });
+    assertEquals(rendered, expected);
+  });
+});
