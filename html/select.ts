4bb808f6af40f468f8c02638f5790a7b449c7c95
Falk Hoppe <falk@hey.com>
2023-06-05 22:41:17 +0200

    fix: add missing attributes to select

--- html/select.ts
+++ html/select.ts
@@ -1,8 +1,27 @@
+import { Format } from "https://deno.land/x/esbuild@v0.17.19/mod.js";
 import { html, HTMLTemplate } from "../deps.ts";
 import { attributeList } from "./element_helper.ts";
 import { HTMLGlobalAttributes } from "./global_attributes.ts";
+import {
+  AutocompleteAttribute,
+  DisabledAttribute,
+  FormAttribute,
+  MultipleAttribute,
+  NameAttribute,
+  RequiredAttribute,
+  SizeAttribute,
+} from "./input_attributes.ts";
 
-export type SelectAttributes = HTMLGlobalAttributes;
+export type SelectAttributes =
+  | RequiredAttribute
+  | AutocompleteAttribute
+  | { autofocus?: boolean }
+  | DisabledAttribute
+  | FormAttribute
+  | MultipleAttribute
+  | NameAttribute
+  | SizeAttribute
+  | HTMLGlobalAttributes;
 
 export interface SelectElementOptions {
   classes?: string[];
--- html/select_test.ts
+++ html/select_test.ts
@@ -16,11 +16,21 @@ Deno.test("select tag element", async (t) => {
     const actual = select(
       "Content",
       {
-        attributes: {},
+        attributes: {
+          autocomplete: "on",
+          autofocus: true,
+          disabled: true,
+          form: "form",
+          multiple: true,
+          name: "name",
+          required: true,
+          size: 1,
+        },
       },
     );
 
-    const expected = `<select>Content</select>`;
+    const expected =
+      `<select autocomplete="on" autofocus disabled form="form" multiple name="name" required size="1">Content</select>`;
     const rendered = await renderToString(actual, { minify: true });
     assertEquals(rendered, expected);
   });
