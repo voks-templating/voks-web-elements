74085406b9516e02b7728338472472d71123e32f
Falk Hoppe <falk@hey.com>
2023-06-05 23:09:26 +0200

    fix: add missing attributes to track

--- html/track.ts
+++ html/track.ts
@@ -2,7 +2,13 @@ import { html, HTMLTemplate } from "../deps.ts";
 import { attributeList } from "./element_helper.ts";
 import { HTMLGlobalAttributes } from "./global_attributes.ts";
 
-export type TrackAttributes = HTMLGlobalAttributes;
+export type TrackAttributes = {
+  default?: boolean;
+  kind?: string;
+  description?: string;
+  chapters?: string;
+  metadata?: string;
+} | HTMLGlobalAttributes;
 
 export interface TrackElementOptions {
   classes?: string[];
@@ -12,8 +18,7 @@ export interface TrackElementOptions {
 export const track = (
   content: string | HTMLTemplate,
   { attributes, classes }: TrackElementOptions = {},
-) =>html`<track ${
-  attributeList<TrackAttributes>(attributes, classes)
-}>${content}</track>`;
-
-  
+) =>
+  html`<track ${
+    attributeList<TrackAttributes>(attributes, classes)
+  }>${content}</track>`;
--- html/track_test.ts
+++ html/track_test.ts
@@ -5,7 +5,7 @@ import { track } from "./track.ts";
 Deno.test("track tag element", async (t) => {
   await t.step("track without attributes", async () => {
     const actual = track("Content");
-    
+
     const expected = `<track>Content</track>`;
 
     const rendered = await renderToString(actual, { minify: true });
@@ -13,18 +13,22 @@ Deno.test("track tag element", async (t) => {
   });
 
   await t.step("track with attributes", async () => {
-
     const actual = track(
-    "Content",
+      "Content",
       {
-        attributes: {},
+        attributes: {
+          default: true,
+          kind: "subtitles",
+          description: "test",
+          chapters: "test",
+          metadata: "test",
+        },
       },
     );
 
-    
-    const expected = `<track>Content</track>`;
+    const expected =
+      `<track default kind="subtitles" description="test" chapters="test" metadata="test">Content</track>`;
     const rendered = await renderToString(actual, { minify: true });
     assertEquals(rendered, expected);
   });
 });
-  
