d3851941a857fa26edd693b1628707dbb940e1b0
Falk Hoppe <falk@hey.com>
2023-06-05 15:50:56 +0200

    feat: add code element

---
+++ html/code.ts
@@ -0,0 +1,19 @@
+import { html, HTMLTemplate } from "../deps.ts";
+import { attributeList } from "./element_helper.ts";
+import { HTMLGlobalAttributes } from "./global_attributes.ts";
+
+export type CodeAttributes = HTMLGlobalAttributes;
+
+export interface CodeElementOptions {
+  classes?: string[];
+  attributes?: CodeAttributes;
+}
+
+export const code = (
+  content: string | HTMLTemplate,
+  { attributes, classes }: CodeElementOptions = {},
+) =>html`<code ${
+  attributeList<CodeAttributes>(attributes, classes)
+}>${content}</code>`;
+
+  
---
+++ html/code_test.ts
@@ -0,0 +1,30 @@
+import { assertEquals } from "asserts";
+import { renderToString } from "../deps.ts";
+import { code } from "./code.ts";
+
+Deno.test("code tag element", async (t) => {
+  await t.step("code without attributes", async () => {
+    const actual = code("Content");
+    
+    const expected = `<code>Content</code>`;
+
+    const rendered = await renderToString(actual, { minify: true });
+    assertEquals(rendered, expected);
+  });
+
+  await t.step("code with attributes", async () => {
+
+    const actual = code(
+    "Content",
+      {
+        attributes: {},
+      },
+    );
+
+    
+    const expected = `<code>Content</code>`;
+    const rendered = await renderToString(actual, { minify: true });
+    assertEquals(rendered, expected);
+  });
+});
+  
