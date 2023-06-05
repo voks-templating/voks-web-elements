438ce6ef9d70ee46d7fae4f8cde4e02e0373a4e2
Falk Hoppe <falk@hey.com>
2023-06-05 23:12:29 +0200

    fix: add missing attributes to video

--- html/video.ts
+++ html/video.ts
@@ -2,7 +2,19 @@ import { html, HTMLTemplate } from "../deps.ts";
 import { attributeList } from "./element_helper.ts";
 import { HTMLGlobalAttributes } from "./global_attributes.ts";
 
-export type VideoAttributes = HTMLGlobalAttributes;
+export type VideoAttributes = {
+  autoplay?: boolean;
+  controls?: boolean;
+  crossorigin?: string;
+  height?: number;
+  loop?: boolean;
+  muted?: boolean;
+  playsinline?: boolean;
+  poster?: string;
+  preload?: string;
+  src?: string;
+  width?: number;
+} | HTMLGlobalAttributes;
 
 export interface VideoElementOptions {
   classes?: string[];
--- html/video_test.ts
+++ html/video_test.ts
@@ -16,11 +16,24 @@ Deno.test("video tag element", async (t) => {
     const actual = video(
       "Content",
       {
-        attributes: {},
+        attributes: {
+          autoplay: true,
+          controls: true,
+          crossorigin: "anonymous",
+          height: 100,
+          loop: true,
+          muted: true,
+          playsinline: true,
+          poster: "test",
+          preload: "auto",
+          src: "test",
+          width: 100,
+        },
       },
     );
 
-    const expected = `<video>Content</video>`;
+    const expected =
+      `<video autoplay controls crossorigin="anonymous" height="100" loop muted playsinline poster="test" preload="auto" src="test" width="100">Content</video>`;
     const rendered = await renderToString(actual, { minify: true });
     assertEquals(rendered, expected);
   });
