87ed992952b06af07ea39827072cc344389817cd
Falk Hoppe <falk@hey.com>
2023-06-05 22:17:52 +0200

    fix: add missing attributes to object

--- html/object.ts
+++ html/object.ts
@@ -2,7 +2,22 @@ import { html, HTMLTemplate } from "../deps.ts";
 import { attributeList } from "./element_helper.ts";
 import { HTMLGlobalAttributes } from "./global_attributes.ts";
 
-export type ObjectAttributes = HTMLGlobalAttributes;
+export type ObjectAttributes =
+  & ({
+    data: string;
+  } | {
+    type: string;
+  })
+  & (
+    | {
+      form?: string;
+      height?: number;
+      width?: number;
+      name?: string;
+      usemap?: string;
+    }
+    | HTMLGlobalAttributes
+  );
 
 export interface ObjectElementOptions {
   classes?: string[];
@@ -12,8 +27,7 @@ export interface ObjectElementOptions {
 export const object = (
   content: string | HTMLTemplate,
   { attributes, classes }: ObjectElementOptions = {},
-) =>html`<object ${
-  attributeList<ObjectAttributes>(attributes, classes)
-}>${content}</object>`;
-
-  
+) =>
+  html`<object ${
+    attributeList<ObjectAttributes>(attributes, classes)
+  }>${content}</object>`;
--- html/object_test.ts
+++ html/object_test.ts
@@ -5,7 +5,7 @@ import { object } from "./object.ts";
 Deno.test("object tag element", async (t) => {
   await t.step("object without attributes", async () => {
     const actual = object("Content");
-    
+
     const expected = `<object>Content</object>`;
 
     const rendered = await renderToString(actual, { minify: true });
@@ -13,18 +13,24 @@ Deno.test("object tag element", async (t) => {
   });
 
   await t.step("object with attributes", async () => {
-
     const actual = object(
-    "Content",
+      "Content",
       {
-        attributes: {},
+        attributes: {
+          data: "https://example.com",
+          form: "form-id",
+          height: 100,
+          width: 100,
+          name: "object-name",
+          type: "video/mp4",
+          usemap: "#map-id",
+        },
       },
     );
 
-    
-    const expected = `<object>Content</object>`;
+    const expected =
+      `<object data="https://example.com" form="form-id" height="100" width="100" name="object-name" type="video/mp4" usemap="#map-id">Content</object>`;
     const rendered = await renderToString(actual, { minify: true });
     assertEquals(rendered, expected);
   });
 });
-  
