28e5775a0ff7c86f59de32b65d320f6214324d1a
Falk Hoppe <falk@hey.com>
2023-06-05 21:55:36 +0200

    feat: add kbd element

---
+++ html/kbd_test.ts
@@ -0,0 +1,30 @@
+import { assertEquals } from "asserts";
+import { renderToString } from "../deps.ts";
+import { kbd } from "./kbd.ts";
+
+Deno.test("kbd tag element", async (t) => {
+  await t.step("kbd without attributes", async () => {
+    const actual = kbd("Content");
+    
+    const expected = `<kbd>Content</kbd>`;
+
+    const rendered = await renderToString(actual, { minify: true });
+    assertEquals(rendered, expected);
+  });
+
+  await t.step("kbd with attributes", async () => {
+
+    const actual = kbd(
+    "Content",
+      {
+        attributes: {},
+      },
+    );
+
+    
+    const expected = `<kbd>Content</kbd>`;
+    const rendered = await renderToString(actual, { minify: true });
+    assertEquals(rendered, expected);
+  });
+});
+  
