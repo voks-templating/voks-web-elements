344e640db39118602f90f0b6d60773887eaa0655
Falk Hoppe <falk@hey.com>
2023-07-03 16:33:31 +0200

    fix!: tr is not an self-closing element
    BREAKING CHANGE: tr accepts now a content element as first param

--- html/tr.ts
+++ html/tr.ts
@@ -1,9 +1,10 @@
-import { html } from "../deps.ts";
+import { html, HTMLTemplate } from "../deps.ts";
 import { attributeList } from "./element_helper.ts";
 import { HTMLGlobalAttributes } from "./global_attributes.ts";
 
 export type TrAttributes = HTMLGlobalAttributes;
 
 export const tr = (
+  content: string | HTMLTemplate,
   attributes: TrAttributes = {},
-) => html`<tr ${attributeList<TrAttributes>(attributes)} \\>`;
+) => html`<tr ${attributeList<TrAttributes>(attributes)}>${content}</tr>`;
--- html/tr_test.ts
+++ html/tr_test.ts
@@ -4,19 +4,28 @@ import { tr } from "./tr.ts";
 
 Deno.test("tr tag element", async (t) => {
   await t.step("tr without attributes", async () => {
-    const actual = tr();
-    const expected = `<tr \\>`;
+    const actual = tr("");
+    const expected = `<tr></tr>`;
 
     const rendered = minify(await renderToString(actual));
     assertEquals(rendered, expected);
   });
 
   await t.step("tr with attributes", async () => {
+    const actual = tr("", {});
+
+    const expected = `<tr></tr>`;
+    const rendered = minify(await renderToString(actual));
+    assertEquals(rendered, expected);
+  });
+
+  await t.step("tr with attributes", async () => {
     const actual = tr(
+      "Content",
       {},
     );
 
-    const expected = `<tr \\>`;
+    const expected = `<tr>Content</tr>`;
     const rendered = minify(await renderToString(actual));
     assertEquals(rendered, expected);
   });
