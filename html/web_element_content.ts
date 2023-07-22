742d3fa649bb6d341b0cb7568c0cc173cafb67f1
Falk Hoppe <falk@hey.com>
2023-07-22 20:42:29 +0200

    fix: allow undefined elements in child element list

--- html/web_element_content.ts
+++ html/web_element_content.ts
@@ -1,6 +1,7 @@
 import { HTMLTemplate, HTMLTemplateGenerator } from "../deps.ts";
 
 export type WebElementContent =
+  | undefined
   | string
   | HTMLTemplate
   | (string | HTMLTemplate | HTMLTemplateGenerator)[];
