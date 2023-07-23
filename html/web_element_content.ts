e49958ab9476444bb1951090538616fa5f909a42
Falk Hoppe <falk@hey.com>
2023-07-23 10:24:38 +0200

    fix: also allow other nilish types

--- html/web_element_content.ts
+++ html/web_element_content.ts
@@ -2,6 +2,8 @@ import { HTMLTemplate, HTMLTemplateGenerator } from "../deps.ts";
 
 export type WebElementContent =
   | undefined
+  | null
+  | false
   | string
   | HTMLTemplate
   | (string | HTMLTemplate | HTMLTemplateGenerator)[];
