50621837f414364fe23526af53cc1fc13c5056de
Falk Hoppe <falk.hoppe@innoq.com>
2024-04-01 12:02:32 +0200

    fix: export utils

--- mod.ts
+++ mod.ts
@@ -114,3 +114,4 @@ export * from "./html/tr.ts";
 export * from "./html/doctype.ts";
 export * from "./html/web_element_content.ts";
 export * from "./html/element_helper.ts";
+export * from "./lib/util.ts";
